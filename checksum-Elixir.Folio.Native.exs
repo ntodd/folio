e1ff5fbf10d519f34d9ea42640b3d82abebcead6
Danila Poyarkov <dev@dannote.net>
2026-05-12 19:39:30 +0300

    Update precompiled NIF checksums for v0.3.1

--- checksum-Elixir.Folio.Native.exs
+++ checksum-Elixir.Folio.Native.exs
@@ -1,6 +1,6 @@
 %{
-  "libfolio_nif-v0.3.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1eae75cffc16c20c9cf86b519fef5c1a064962ba342cd34d13b110e955f9b1b5",
-  "libfolio_nif-v0.3.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:db8533d04aff2fb691a19f8309012603c2df0b7a157d4e42042fd711e6ac4cae",
-  "libfolio_nif-v0.3.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:9fa5fb2ce2b026964ab13df8760ce4f3142c647a4e0560d5603f54e0828e5160",
-  "libfolio_nif-v0.3.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f834ff3657993a1a13cbeca49828b53b35488d5e95a5192c221dc4980d23ed76",
+  "libfolio_nif-v0.3.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:4f8664c5d248d8ff182a88240258202c6e32924197b82d26854f528ff8a702e5",
+  "libfolio_nif-v0.3.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:e15d4aaa396262eeb630a50336d101d36eb8e07fc2addd401194be8645b37aff",
+  "libfolio_nif-v0.3.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:56dd009fdd104dc74afa96836cdf778c6178cf9cb1c6d295754f91667fd0ce64",
+  "libfolio_nif-v0.3.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:50046d13d7038fab1344cc153e113be347f7c7ef30804e52b6e4df55b3bd2ba9",
 }
